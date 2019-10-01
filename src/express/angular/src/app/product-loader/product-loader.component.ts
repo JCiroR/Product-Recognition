54ea421006ae937c398a85a05a881b17eaf9f92d
Hamilton Tobon <hamiltontobon77@gmail.com>
2019-10-01 14:48:15 -0500

    Skeleton of product loader working

--- src/express/angular/src/app/product-loader/product-loader.component.ts
+++ src/express/angular/src/app/product-loader/product-loader.component.ts
@@ -18,15 +18,17 @@ export class ProductLoaderComponent implements OnInit {
     ngOnInit() {
     }
 
+    // Activated when the user selects an image.
     onImageSelected(event) {
         this.selectedImage = <File>event.target.files[0];
     }
 
+    // Activated when the user wants to send the image to the server.
+    // That is, the user presses the button 'Upload'.
     onUpload() {
         const fd: FormData = new FormData();
         fd.append('image', this.selectedImage, this.selectedImage.name);
-        var product = this.productLoaderService.sendImage(fd);        
-        console.log(product);
+        var product = this.productLoaderService.sendImage(fd);
         product.then(product => console.log(product))
             .catch(err => console.log(err));
     }
