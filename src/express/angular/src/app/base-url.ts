9869dfa0efd9f30ca14d840ab1fcd23bddcf46a7
Hamilton Tobon Mosquera <hamiltontobon77@gmail.com>
2019-10-19 10:20:55 -0500

    Changing to test environment

--- src/express/angular/src/app/base-url.ts
+++ src/express/angular/src/app/base-url.ts
@@ -1,1 +1,2 @@
-export var baseUrl: string = 'http://whlab.dis.eafit.edu.co/api';
+// export var baseUrl: string = 'http://whlab.dis.eafit.edu.co/api';
+export var baseUrl: string = 'http://localhost/api:3000';
