ba557606d74abc7b8342d5d3cd0d0d6cacd9c9d3
JCiroR <jcirore@eafit.edu.co>
2019-10-22 16:55:36 -0500

    Clean api.js

--- src/express/server/routes/api.js
+++ src/express/server/routes/api.js
@@ -1,15 +1,6 @@
 var tensorflow = require('../tensorflow/tensorflow.js');
 
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////DOCUMENTOS//////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////
-
-
 const multer = require("multer");
-var fs = require('fs');
-const path = require('path');
 
 var upload = multer({ storage: multer.diskStorage({
 
@@ -22,20 +13,12 @@ var upload = multer({ storage: multer.diskStorage({
         cb(null, (err ? undefined : raw.toString('hex') ) + ext);
       });
     }
-
 })});
 
 
 module.exports = (app) => {
-	// Ruta inicial
-	app.get('/api', (req, res) => {
-		console.log(__dirname)
-		res.sendFile('uploadfile.html', {root: __dirname});
-	});
-
 	app.post('/api/image', upload.single("image"), (req, res) => {
 		const tempPath = req.file.path
 		tensorflow.init(tempPath, res);
 	});
-
 };
